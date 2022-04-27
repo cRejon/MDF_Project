5ad0454cc09508754a94e590c05a2fbd70879db6
cRejon <carlosrejongomez@gmail.com>
2022-04-27 19:36:53 +0200

    Update si1145 test

--- Test/si1145/main.py
+++ Test/si1145/main.py
@@ -2,9 +2,10 @@
 from machine import I2C
 import si1145
 import time
-i2c = I2C(0, I2C.MASTER, baudrate=100000)
+i2c = I2C(0)
+i2c.init(I2C.MASTER, baudrate=100000, pins=('P22','P23'))
 sensor = si1145.SI1145(i2c=i2c)
-for i in range(10):
+while True:
     uv = sensor.read_uv
     ir = sensor.read_ir
     view = sensor.read_visible
