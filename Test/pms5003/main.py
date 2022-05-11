134582633efc962b001314f9cfda1254eee35e61
cRejon <carlosrejongomez@gmail.com>
2022-05-11 15:24:47 +0200

    Add pms5003 test

---
+++ Test/pms5003/main.py
@@ -0,0 +1,14 @@
+import pms5003
+import machine
+import time
+
+
+print("Initing UART")
+uart = machine.UART(1, 9600, parity=None, stop=1, pins=('P9','P10'))
+time.sleep(30)
+print("Initing PMS5003")
+pms = pms5003.PMS5003(uart, set='P11', reset='P12')
+pms.set_pms()
+pm10_standard, pm25_standard, pm100_standard, pm10_env, \
+    pm25_env, pm100_env, particles_03um, particles_05um, \
+    particles_10um, particles_25um, particles_50um, particles_100um  = pms.read_pms()
