b607e1429f18a52dfdcc1c9a29c3dafc416b603a
cRejon <carlosrejongomez@gmail.com>
2022-05-11 15:51:15 +0200

    Update pins in pms5003 test

--- Test/pms5003/main.py
+++ Test/pms5003/main.py
@@ -4,10 +4,10 @@ import time
 
 
 print("Initing UART")
-uart = machine.UART(1, 9600, parity=None, stop=1, pins=('P9','P10'))
+uart = machine.UART(1, 9600, parity=None, stop=1, pins=('P15','P14'))
 time.sleep(30)
 print("Initing PMS5003")
-pms = pms5003.PMS5003(uart, set='P11', reset='P12')
+pms = pms5003.PMS5003(uart, set='P17', reset='P16')
 pms.set_pms()
 pm10_standard, pm25_standard, pm100_standard, pm10_env, \
     pm25_env, pm100_env, particles_03um, particles_05um, \
