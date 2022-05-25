3c96ee6562c3a0b6836b0feacd9af864ec7f79eb
cRejon <carlosrejongomez@gmail.com>
2022-05-25 21:19:08 +0200

    Change pms5003 library

--- Test/pms5003/lib/pms5003.py
+++ Test/pms5003/lib/pms5003.py
@@ -1,69 +1,149 @@
-import time
+from machine import Pin
 import struct
-from machine import UART, Pin
+import time
+
+
+PMS5003_SOF = bytearray(b'\x42\x4d')
+
+
+class ChecksumMismatchError(RuntimeError):
+    pass
+
+
+class ReadTimeoutError(RuntimeError):
+    pass
+
+
+class SerialTimeoutError(RuntimeError):
+    pass
+
+
+class PMS5003Data():
+    def __init__(self, raw_data):
+        self.raw_data = raw_data
+        self.data = struct.unpack(">HHHHHHHHHHHHHH", raw_data)
+        self.checksum = self.data[13]
+
+    def pm_ug_per_m3(self, size, atmospheric_environment=False):
+        if atmospheric_environment:
+            if size == 1.0:
+                return self.data[3]
+            if size == 2.5:
+                return self.data[4]
+            if size is None:
+                return self.data[5]
+
+        else:
+            if size == 1.0:
+                return self.data[0]
+            if size == 2.5:
+                return self.data[1]
+            if size == 10:
+                return self.data[2]
+
+        raise ValueError("Particle size {} measurement not available.".format(size))
+
+    def pm_per_1l_air(self, size):
+        if size == 0.3:
+            return self.data[6]
+        if size == 0.5:
+            return self.data[7]
+        if size == 1.0:
+            return self.data[8]
+        if size == 2.5:
+            return self.data[9]
+        if size == 5:
+            return self.data[10]
+        if size == 10:
+            return self.data[11]
+
+        raise ValueError("Particle size {} measurement not available.".format(size))
+
+    def __repr__(self):
+        return """
+PM1.0 ug/m3 (ultrafine particles):                             {}
+PM2.5 ug/m3 (combustion particles, organic compounds, metals): {}
+PM10 ug/m3  (dust, pollen, mould spores):                      {}
+PM1.0 ug/m3 (atmos env):                                       {}
+PM2.5 ug/m3 (atmos env):                                       {}
+PM10 ug/m3 (atmos env):                                        {}
+>0.3um in 0.1L air:                                            {}
+>0.5um in 0.1L air:                                            {}
+>1.0um in 0.1L air:                                            {}
+>2.5um in 0.1L air:                                            {}
+>5.0um in 0.1L air:                                            {}
+>10um in 0.1L air:                                             {}
+""".format(*self.data[:-2], checksum=self.checksum)
+
+    def __str__(self):
+        return self.__repr__()
+
 
 class PMS5003():
-    def __init__(self, uart, set='P11', reset='P12'):
-        """
-            Lopy     :: PMS
-            TXD: P9  :: RXD: P4
-            RXD: P10 :: TXD: P5
-            SET: P11 :: SET: P3
-            RESET: P12 :: RESET: P6
-        """
-        self.STARTUP_TIME = 30   # startup time
-        self.port = uart
-        self.set_pin = Pin(set, mode=Pin.OUT)
-        self.reset_pin = Pin(reset, mode=Pin.OUT)
+    def __init__(self, uart=1, pin_enable='P17', pin_reset='P16'):
+        self._serial = uart
+        self._pin_enable = Pin(pin_enable, mode=Pin.OUT)
+        self._pin_reset = Pin(pin_reset, mode=Pin.OUT)
+        self.setup()
 
-        self.set_pin.value(0)  # start powered off
-        self.reset_pin.value(1)  # pull low and high for reset
+    def setup(self):
+        self._pin_enable.value(1)
+        self._pin_reset.value(1)
 
-    def set_pms(self):
-        print("PMS: set")
-        self.set_pin.value(1)
-        time.sleep(self.STARTUP_TIME)
+        self.reset()
 
-    def reset_pms(self):
-        print("PMS: reset")
-        self.reset_pin.value(0)
-        time.sleep_ms(50)
-        self.reset_pin.value(1)
+    def reset(self):
+        time.sleep(0.1)
+        self._pin_reset.value(0)
+        #self._serial.flushInput()
+        time.sleep(0.1)
+        self._pin_reset.value(1)
 
+    # added
     def sleep(self):
-        print("PMS: sleep")
-        self.set_pin.value(0)
+        self._pin_enable.value(0)
 
-    def packet_from_data(self, data):
-        numbers = struct.unpack('>16H', data)
-        csum = sum(data[:-2])
-        if csum != numbers[-1]:
-            print("Bad packet data: %s / %s", data, csum)
-            return None
-        return numbers[2:-2]
+    def read(self):
+        start = time.time()
 
-    def read_pms(self): 
-        c = self.port.read(1)
-        print(c)
-        if c != '\x42':
-            self.read_pms()
+        sof_index = 0
 
-        c = self.port.read(1)
-        print(c)
-        if c != '\x4d':
-            self.read_pms()
+        while True:
+            elapsed = time.time() - start
+            if elapsed > 5:
+                raise ReadTimeoutError("PMS5003 Read Timeout: Could not find start of frame")
 
-        data = bytearray((0x42, 0x4d))
-        print('data:', len(data), '\n', data)
+            sof = self._serial.read(1)
+            print(sof)
+            if sof == None:
+                raise SerialTimeoutError("PMS5003 Read Timeout: Failed to read start of frame byte")
+            sof = ord(sof) if type(sof) is bytes else sof
 
-        data += self.port.read(30)
-        print('data:', len(data), '\n', data)
+            if sof == PMS5003_SOF[sof_index]:
+                if sof_index == 0:
+                    sof_index = 1
+                elif sof_index == 1:
+                    break
+            else:
+                sof_index = 0
 
-        if len(data) != 32:
-            self.read_pms()
+        checksum = sum(PMS5003_SOF)
 
-        measurements = self.packet_from_data(data)
-        if measurements == None:
-            print("PMS: read failed")
-        else:
-            return measurements
+        data = bytearray(self._serial.read(2))  # Get frame length packet
+        if len(data) != 2:
+            raise SerialTimeoutError("PMS5003 Read Timeout: Could not find length packet")
+        checksum += sum(data)
+        frame_length = struct.unpack(">H", data)[0]
+
+        raw_data = bytearray(self._serial.read(frame_length))
+        if len(raw_data) != frame_length:
+            raise SerialTimeoutError("PMS5003 Read Timeout: Invalid frame length. Got {} bytes, expected {}.".format(len(raw_data), frame_length))
+
+        data = PMS5003Data(raw_data)
+        # Don't include the checksum bytes in the checksum calculation
+        checksum += sum(raw_data[:-2])
+
+        if checksum != data.checksum:
+            raise ChecksumMismatchError("PMS5003 Checksum Mismatch {} != {}".format(checksum, data.checksum))
+
+        return data
--- Test/pms5003/main.py
+++ Test/pms5003/main.py
@@ -1,17 +1,15 @@
-import pms5003
-import machine
+from machine import UART
+from pms5003 import PMS5003
 import time
 
+uart = UART(1, 9600, parity=None, stop=1, pins=('P14','P15'))
 
-print("Initing UART")
-uart = machine.UART(1, 9600, parity=None, stop=1, pins=('P15','P14'))
+# Configure the PMS5003 
+pms5003 = PMS5003(uart= uart, pin_enable='P17', pin_reset='P16')
 time.sleep(30)
-print("Initing PMS5003")
-pms = pms5003.PMS5003(uart, set='P17', reset='P16')
-pms.set_pms()
-pm10_standard, pm25_standard, pm100_standard, pm10_env, \
-    pm25_env, pm100_env, particles_03um, particles_05um, \
-    particles_10um, particles_25um, particles_50um, particles_100um  = pms.read_pms()
-
-print("pm25_standard: ", pm25_standard)
-print("pm100_standard: ", pm100_standard)
+while True:
+    data = pms5003.read()
+    print(data)
+    print(data.pm_ug_per_m3(2.5))
+    print(data.pm_ug_per_m3(10))
+    time.sleep(5)
