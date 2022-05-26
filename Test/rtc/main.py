2bc8f07a3084c8e439c1dbf97661c0792a1b39c2
cRejon <carlosrejongomez@gmail.com>
2022-05-26 15:04:03 +0200

    Change RTC test to get timestamp

--- Test/rtc/main.py
+++ Test/rtc/main.py
@@ -1,12 +1,20 @@
 import time
-from machine import I2C
+from machine import I2C, RTC
 import ds3231
 
 i2c = I2C(0)
 i2c.init(I2C.MASTER, baudrate=100000, pins=('P22','P23'))
 
-ds = ds3231.DS3231(i2c)
+ds3231 = ds3231.DS3231(i2c)
+
+# ds3231 doesn't give timestamp
+rtc = RTC()
+print(ds3231.DateTime())
+rtc.init(tuple(ds3231.DateTime()+[0, 0])) # [microseconds, tzinfo (ignored in microPython)]
 
 while True:
-    print(ds.DateTime())
+    now = rtc.now()
+    print("Now: ", now)
+    timestamp = time.time()
+    print("Timestamp: ", timestamp)
     time.sleep(5)
